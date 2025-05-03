e998dc0e82217167ceb639ee085989d1006d5057
domipm <dpas602@gmail.com>
2025-05-03 13:49:23 +0200

    Improved code, included contrast control, clean up

--- vq_vae_train.py
+++ vq_vae_train.py
@@ -6,30 +6,51 @@ import  torch
 import  shutil
 import  torchvision
 
-import  torch.nn                    as nn
-import  torch.optim                 as optim
+import  torch.nn                    as  nn
+import  torch.optim                 as  optim
 
-import  numpy                       as np
-import  matplotlib.pyplot           as plt
+import  numpy                       as  np
+import  matplotlib.pyplot           as  plt
 
 from    torch.utils.data            import DataLoader, random_split
 from    torchvision.transforms      import v2
-
-
 
 from    vq_vae                      import VQ_VAE, FramesDataset
 
 
 
-'''DATASET LOADING'''
+'''HYPERPARAMETERS'''
 
 
+
+# Name of the game (in this case, testing dataset)
+fname = "Pong-v5"
 
 # Batch size
 batch_size = 16
+# Image dimensions (rescaled)
+img_dims = ((128, 64))
+# Contrast adjustment factor (1 = no adjustment)
+contrast_fact = 1
 
-# Name of the game (in this case, testing dataset)
-fname = "Breakout-v5"
+# Codebook dimension
+codebook_num = 512
+codebook_dim = 64
+# Codebook loss weights
+codebook_loss = 1
+commit_loss   = 1
+
+# Training parameters
+epochs          = 50
+learning_rate   = 0.001
+# Save weights every n-th epoch
+save_weights    = 10  
+
+
+
+'''DATASET LOADING & VQ-VAE INITIALIZATION'''
+
+
 
 # Path to frames
 path_frames = "./frames/" + fname + "/"
@@ -43,12 +64,22 @@ if os.path.isdir(path_out):
 # Create empty directory
 os.mkdir(path_out)
 
+# Contrast adjustment class
+class AdjustContrast:
+    def __init__(self, factor):
+        self.factor = factor
+
+    def __call__(self, img):
+        return v2.functional.adjust_contrast(img, self.factor)
+
 # Transform object to apply to frames
 transform_frames = v2.Compose([
     # Resize all images (square shape or divisible by 2!)
-    v2.Resize((128, 128)),
+    v2.Resize(img_dims),
+    # Adjust contrast
+    AdjustContrast(factor = contrast_fact),
     # Convert to grayscale
-    v2.Grayscale(),
+    # v2.Grayscale(),
     # Convert to tensor object
     v2.ToImage(),
     v2.ToDtype(torch.float32, scale = True),
@@ -62,7 +93,7 @@ dataset = FramesDataset(path_frames, transform_frames)
 # Take single frame from dataset and repeat for batch size (as input shape)
 image_shape = next(iter(dataset)).unsqueeze(0).repeat(batch_size, 1, 1, 1).shape
 
-# Use random_split to create train/test subsets
+# Use random_split to create train/test subsets (95% train, 5% test)
 dataset_train, dataset_test = random_split(dataset, [0.95, 0.05])
 
 # Load train images from dataset batch-wise using dataloader
@@ -71,11 +102,11 @@ dataloader_train = DataLoader(dataset_train, batch_size = batch_size, shuffle = 
 dataloader_test = DataLoader(dataset_test, batch_size = batch_size, shuffle = True)
 
 # Initialize VQ-VAE model
-model = VQ_VAE( embedding_num=512,
-                embedding_dim=64,
-                l_codebook=1,
-                l_commit=1,
-                in_shape=image_shape   
+model = VQ_VAE( embedding_num = codebook_num,
+                embedding_dim = codebook_dim,
+                l_codebook    = codebook_loss,
+                l_commit      = commit_loss,
+                in_shape      = image_shape   
 )
 
 
@@ -85,13 +116,7 @@ model = VQ_VAE( embedding_num=512,
 
 
 # Print info on screen
-print("\nVQ-VAE Training for", fname)
-
-# Training parameters
-epochs          = 2
-learning_rate   = 0.001
-# Save weights every n-th epoch
-save_weights    = 25         
+print("\nVQ-VAE Training for", fname)   
 
 # Define optimizer
 optimizer = optim.Adam(model.parameters(), lr = learning_rate)
@@ -103,6 +128,9 @@ loss_arr = []
 
 # Main training loop
 for epoch in range(1, epochs + 1):
+
+    # Set model to train
+    model.train()
 
     print("\n", "*"*6, "Epoch ", epoch, " ", "*"*6)
 
@@ -119,11 +147,8 @@ for epoch in range(1, epochs + 1):
         loss_recon = criterion(out, image)
         loss = loss_recon + loss_vq
 
-        # Append loss to array
-        loss_arr.append( [loss_recon.item(), loss_vq.item(), loss.item()] )
-
         # Print and keep track of loss value
-        print("Loss = ", loss.item(), end = "\r")
+        print("Loss = {:.17f}".format(loss.item()), end = "\r")
 
         # Backpropagate errors backward through network
         loss.backward()
@@ -132,6 +157,9 @@ for epoch in range(1, epochs + 1):
 
     # Obtain evaluation loss
 
+    # Append final loss of current epoch
+    loss_arr.append( [loss_recon.item(), loss_vq.item(), loss.item()] )
+
     # Save the weights of the model every n-th epochs
     if epoch % save_weights == 0:
         torch.save(model, f = path_out + "weights_" + str(epoch) + ".pt")
@@ -139,7 +167,7 @@ for epoch in range(1, epochs + 1):
 # Print info on screen
 print("\n\nCompleted!\n")
 # Write loss array to file
-np.save(path_out + "loss", arr = loss_arr)
+np.save(path_out + "loss_log", arr = loss_arr)
 
 
 
@@ -163,8 +191,8 @@ images = next(iter(dataloader_test))
 grid_img = torchvision.utils.make_grid(images, nrow=4)
 # Convert tensor object to numpy array
 npimg = grid_img.permute(1, 2, 0).numpy()
-# Plot image
-ax[0].imshow(npimg)
+# Plot image (scale by 255 and convert to uint8)
+ax[0].imshow((npimg * 255).astype(np.uint8))
 
 # Set model to evaluation mode
 model.eval()
@@ -176,8 +204,8 @@ reconstructed, _, loss = model(images)
 grid_img = torchvision.utils.make_grid(reconstructed.detach(), nrow=4)
 # Convert tensor object to numpy array
 npimg = grid_img.permute(1, 2, 0).numpy()
-# Plot image
-ax[1].imshow(npimg/np.amax(npimg))
+# Plot image (scale by 255 and convert to uint8)
+ax[1].imshow((npimg*255).astype(np.uint8))
 
 # Setup narrow margins and save image
 plt.tight_layout()
@@ -192,7 +220,7 @@ plt.close()
 
 
 # Load loss output 
-loss = np.load(path_out + "/loss.npy")
+loss = np.load(path_out + "/loss_log.npy")
 
 # Define labels for legend
 labels = [r"$\mathcal{L}_{\text{Recon}}$", r"$\mathcal{L}_{\text{VQ}}$", r"$\mathcal{L}_{\text{Total}}$"]
@@ -208,6 +236,6 @@ plt.ylabel("Loss")
 # Save plot
 plt.legend()
 plt.tight_layout()
-plt.savefig(path_out + "loss_log.pdf")
+plt.savefig(path_out + "loss_evol.pdf")
 # Close plot
 plt.close()
