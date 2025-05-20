f20ad4ad0dc3eade847bc1a5fc4f44cad8ca3b6f
domipm <dpas602@gmail.com>
2025-05-20 12:04:35 +0200

    Removed unused libraries, load weights based on max vqvae, minor fixes

--- visualizations.py
+++ visualizations.py
@@ -7,7 +7,7 @@ import  numpy                       as      np
 import  matplotlib.pyplot           as      plt
 
 from    PIL                         import  Image
-from    torch.utils.data            import  DataLoader, random_split
+from    torch.utils.data            import  DataLoader
 from    torchvision.transforms      import  v2, InterpolationMode
 
 from    vq_vae                      import  VQ_VAE
@@ -15,8 +15,12 @@ from    dataloader                  import  FramesDataset, CustomMarginCrop, get
 
 
 
+'''PARAMETERS'''
+
+
+
 # Game name
-fname = "Boxing-v5"
+fname = "Pong-v5"
 
 # Path to frames
 path_frames = f"./frames/{fname}/"
@@ -42,15 +46,32 @@ image_org = Image.open(path_frames + images_fnames[42]).convert('L')
 # Convert to numpy array
 image_org_arr = np.array(image_org)
 
+# Get maximum (latest) model weights
+file_weights = []
+for file in os.listdir("./output/" + fname + "/"):
+    if file.endswith(".pt") and file.startswith("vqvae"):
+        file_name = file.split(".")[0].split("_")[2]
+        file_weights.append(int(file_name))
+file_weights = np.max(file_weights)
+
 # Load model
-model = torch.load(f"{path_out}vqvae_weights_50.pt", weights_only = False)
+model = torch.load(f"{path_out}vqvae_weights_{file_weights}.pt", weights_only = False)
 model.eval()  # Set model to evaluation mode
 
 # Compute margins based on dynamic map of frames
 motion, l, r, t, b = get_margins(path_frames = path_frames)
 
 # Transform object to apply to frames
-transform_frames = v2.Compose([
+transform_org = v2.Compose([
+    # Convert to grayscale
+    v2.Grayscale(),
+    # Convert to tensor object
+    v2.ToImage(),
+    v2.ToDtype(torch.float32, scale = True),
+])
+
+# Transform object to apply to frames
+transform_pre = v2.Compose([
     # Convert to grayscale
     v2.Grayscale(),
     # Custom crop
@@ -59,17 +80,16 @@ transform_frames = v2.Compose([
     v2.ToImage(),
     v2.ToDtype(torch.float32, scale = True),
     # Resize all images (square shape or divisible by 2!)
-    v2.Resize( img_dims , interpolation = InterpolationMode.NEAREST, antialias = False),
+    v2.Resize( img_dims , interpolation = InterpolationMode.NEAREST_EXACT, antialias = False),
 ])
 
-# Load dataset using custom dataloader
-dataset = FramesDataset("./frames/" + fname, transform_frames)
+# Load dataset (no transform)
+dataset_org = FramesDataset("./frames/" + fname, transform_org)
+# Load dataset (pre-processed)
+dataset_pre = FramesDataset("./frames/" + fname, transform_pre)
 
-# Load train images from dataset batch-wise using dataloader
-dataloader_train = DataLoader(dataset, batch_size = 16, shuffle = False)
-
-# Take batch
-image = next(iter(dataloader_train)) # .unsqueeze(0).repeat(16, 1, 1, 1)
+# Create batch dataloader (pre-processed)
+dataloader_pre = DataLoader(dataset_pre, batch_size = 16, shuffle = False)
 
 
 
@@ -77,38 +97,38 @@ image = next(iter(dataloader_train)) # .unsqueeze(0).repeat(16, 1, 1, 1)
 
 
 
+# Load batch of images
+image = next(iter(dataloader_pre))
+
 # Initialize autoencoder
 autoenc = VQ_VAE(embedding_num, embedding_dim, beta, image.shape)
-
+# Set evaluation mode
 autoenc.eval()
-
+# Ensure no gradients computed
 with torch.no_grad():
+    # Preprocess, encode, and quantize image
     z_e, indices, _ = autoenc.encode_and_quantize(x = image)
 
 # Reshape back to 2D
-indices_re = indices.view(batch_size, z_e.shape[0], z_e.shape[0]).detach().numpy()
+indices_re = indices.view(batch_size, z_e.shape[2], z_e.shape[2]).detach().numpy()
 
-nrow = 4
+n_img = 4
+n_col = 2
 
-B, H, W = indices_re.shape
-ncol = (B + nrow - 1) // nrow  # Compute number of rows based on total batch size
+fig, ax = plt.subplots(nrows = n_col, ncols = n_img)
 
-fig, axes = plt.subplots(ncol, nrow, figsize=(nrow * 3, ncol * 3))
-axes = axes.flatten()
-
-for i in range(B):
-    ax = axes[i]
-    ax.imshow(indices_re[i], cmap='viridis')
-    ax.set_title(f"Sample {i}")
-    ax.axis('off')
-
-# Hide unused subplots
-for j in range(B, len(axes)):
-    axes[j].axis('off')
+# Plot originals
+for i in range(n_img):
+        ax[0,i].imshow(image[i][0], cmap = 'viridis')
+        ax[0,i].axis('off')
+# Plot codebook 
+for i in range(n_img):
+        ax[1,i].imshow(indices_re[i], cmap = 'viridis')
+        ax[1,i].axis('off')
 
 plt.tight_layout()
 plt.savefig(f"./output/{fname}/vqvae_codebook.pdf")
-# plt.show()
+
 plt.close()
 
 
@@ -119,9 +139,6 @@ plt.close()
 
 # Setup plots
 fig, ax = plt.subplots(nrows = 1, ncols = 3)
-
-# Compute pre-processed image
-image_post = dataset[42].detach().numpy()[0]
 
 # Remove axis lines
 ax[0].axis('off')
@@ -134,9 +151,9 @@ ax[1].set_title("Motion Map & Margins")
 ax[2].set_title("Pre-Processed Img.")
 
 # Plot
-ax[0].imshow(image_org_arr, cmap = "gray")
+ax[0].imshow(dataset_org[42][0], cmap = "gray")
 ax[1].imshow(motion, cmap = "gray")
-ax[2].imshow(image_post, cmap = "gray")
+ax[2].imshow(dataset_pre[42][0], cmap = "gray")
 
 # Boundary lines
 ax[1].vlines(x = l, ymin = 0, ymax = 210, color="green", linewidth=1)
