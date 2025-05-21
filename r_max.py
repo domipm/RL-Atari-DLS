db2ba61f457d664de8e8993bb8e1b15cfbb80431
domipm <dpas602@gmail.com>
2025-05-21 19:58:18 +0200

    Rewritten code, implemented transforms, small improvements (not working)

--- r_max.py
+++ r_max.py
@@ -1,10 +1,14 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
 import  os
-import  time
 import  ale_py
 
 import  numpy               as  np
 import  gymnasium           as  gym
 import  matplotlib.pyplot   as  plt
+import  torch.nn.functional as  F
 
 import  torch
 
@@ -23,6 +27,12 @@ import  dataloader
 # Game name
 fname = "Pong-v5"
 
+# Define the environment
+env = gym.make("ALE/" + fname, 
+               render_mode="rgb_array",
+               frameskip = 1, 
+               repeat_action_probability = 0.25)
+
 # Path to output
 path_out    = "./output/" + fname + "/"
 
@@ -30,135 +40,153 @@ path_out    = "./output/" + fname + "/"
 img_dims = ((64, )*2)
 
 # Numer of epochs to train for
-epochs = 250
+epochs              = 250
 
-# Number of states in the state space
-state_space_size = 8000
+# Maximum number of states to contain
+state_space_size    = 5000
+# Maximum steps per episode
+max_episode_steps   = 1000
 
 # RL Parameters
-optimistic_reward = 2.0 
-gamma = 0.99
-max_iters = 10
-tolerance = 1e-5
+optimistic_reward   = 2.0 
+gamma               = 0.99
+max_iters           = 10
+tolerance           = 1e-5
 # Epsilon-greedy
-epsilon = 0.99
-epsilon_decay = 0.95
-epsilon_min = 0.01
+epsilon             = 0.99
+epsilon_decay       = 0.95
+epsilon_min         = 0.01
 # R-Max parameters
-R_max = 1
-m     = 3
-
-# Compute margins based on dynamic map of frames
-_, l, r, t, b = dataloader.get_margins(path_frames = "./frames/" + fname + "/")
-
-# Default transform
-transform_frames = v2.Compose([
-    # Convert to grayscale
-    v2.Grayscale(),
-    # Custom crop
-    dataloader.CustomMarginCrop(l, r, t, b),
-    # Convert to tensor object
-    v2.ToImage(),
-    v2.ToDtype(torch.float32, scale = True),
-    # Resize all images (square shape or divisible by 2!)
-    v2.Resize(img_dims, interpolation = InterpolationMode.NEAREST, antialias = False),
-])
+R_max               = 1
+m                   = 3
 
 
 
-'''R-MAX WRAPPER'''
+'''R-MAX WRAPPER MAP STATE CODES TO INDICES'''
 
 
 
-def detgetMRP(P_sas, R_sa, pi):
+class IndexMapper:
 
-    P_ss = np.squeeze(np.take_along_axis(P_sas, pi.reshape(-1,1,1), axis=1), axis=1)
-    R_s  = np.take_along_axis(R_sa, pi.reshape(-1,1), axis=1)
+    def __init__(self, fname, state_space_size = 0):
 
-    return P_ss,R_s
-
-class PolicyIndexMapper:
-
-    def __init__(self):
-
-        self.state_to_index = {}  # Dictionary to map state code to an index
-        self.index_to_state = {}  # Reverse mapping (for debugging)
+        # Dictionary that maps each codebook state to an index
+        self.state_to_index = {}
+        # Reverse mapping
+        self.index_to_state = {} 
+        # Maximum length of this dictionary
+        self.state_space_size = state_space_size
+        # Keep track of index count
         self.next_index = 0
 
-    def get_index(self, state_code):
+        # Compute margins based on dynamic map of frames
+        _, l, r, t, b = dataloader.get_margins(path_frames = "./frames/" + fname + "/")
 
-        # Check if state_code already has an assigned index
+        # Define default transform (same as VQ-VAE!)
+        self.transform_frames = v2.Compose([
+            # Convert to grayscale
+            v2.Grayscale(),
+            # Custom crop
+            dataloader.CustomMarginCrop(l, r, t, b),
+            # Convert to tensor object
+            v2.ToImage(),
+            v2.ToDtype(torch.float32, scale = True),
+            # Resize all images (square shape or divisible by 2!)
+            v2.Resize(img_dims, interpolation = InterpolationMode.NEAREST, antialias = False),
+        ])
+
+        # Get maximum (latest) model weights
+        file_weights = []
+        for file in os.listdir("./output/" + fname + "/"):
+            if file.endswith(".pt") and file.startswith("vqvae"):
+                file_name = file.split(".")[0].split("_")[2]
+                file_weights.append(int(file_name))
+        file_weights = np.max(file_weights)
+
+        # Import VQ-VAE model
+        model_path = f"{path_out}vqvae_weights_{str(file_weights)}.pt"
+        try:
+            self.model = torch.load(model_path, weights_only = False)
+            self.model.eval()
+            print(f"Loaded model weights from {model_path}")
+        except:
+            print(f"Model weights file {model_path} not found!")
+            exit()
+
+
+
+    def __len__(self):
+        '''Return current length of saved state-index pairs (<= state_space_size)'''
+
+        return len(self.state_to_index)
+
+
+
+    def get_index(self, state_code):
+        '''Returns the index for a given state codebook representation'''
+
+        # Check if it already has assigned index
         if state_code not in self.state_to_index:
+            # Also check if the next index goes out of bounds
+            if self.next_index >= self.state_space_size:
+                # Don't return any index! (ignore this state)
+                return None
+            
+            # If not assigned an index, do it (next index equals length)
             self.state_to_index[state_code] = self.next_index
+            # Add also entry to inverse dictionay
             self.index_to_state[self.next_index] = state_code
+            # Increase next index count
             self.next_index += 1
 
         return self.state_to_index[state_code]
 
-# Function used to obtain the state_id index from tensor image
-def get_index_from_image(img, state_space_size):
-
-    # Pre-process the frames
-    img = transform_frames(img) 
-
-    img = img.unsqueeze(0)  # Add batch dimension (1,3,128,64)
-    # Ensure no gradient calculation
-    with torch.no_grad():
-        _, codebook_idx, _ = model.encode_and_quantize(img) # (1,4,84,84)
-
-    # tuple(codebook_idx.view(-1).cpu().numpy().tolist())) % 5000
-    state_id = hash(codebook_idx.numpy().tobytes())
-
-    return state_id
-
-# Function used to decide action to take according to policy
-def policy_action(state):
-
-    if np.random.rand() < epsilon:
-        return env.action_space.sample()
-    else:
-        return policy[mapper.get_index(state)]
 
 
+    def get_index_from_image(self, img):
+        '''Return state id for a given frame'''
 
-'''INITIALIZATIONS'''
+        # Pre-process the frames and add batch dimension
+        # (C, H, W) ~ (1, 210, 160) -> (1, 1, 64, 64)
+        img = self.transform_frames(img).unsqueeze(0)
+        
+        # Ensure no gradient calculation
+        with torch.no_grad():
+            # Compute output from VQ-VAE for given image
+            # z_e           ~ (1, 32, 32, 32)
+            # codebook_idx  ~ (1, 1, 32, 32)
+            _, codebook_idx, _ = self.model.encode_and_quantize(img)
+
+        # Reshape codebook indices to same dimensions as encoded image
+        codebook_idx = codebook_idx.view(1, 32, 32)
+
+        # Perform max pooling (reduce codebook size)
+        # (1, 1, 32, 32) -> (1, 1, 16, 16)
+        codebook_idx_ds = F.max_pool2d(codebook_idx, kernel_size = 4, stride = 2, padding = 1)
+        # (1, 1, 16, 16) -> (1, 1, 8, 8)
+        # codebook_idx_ds = F.max_pool2d(codebook_idx, kernel_size = 4, stride = 2, padding = 1)
+
+        # Obtain state id as hash, after converting indices to tuple
+        state_id = hash(tuple(codebook_idx_ds.view(-1).cpu().numpy().tolist()))
+
+        # Return computed state id for the frame
+        return state_id
 
 
 
-# Define environment
-env = gym.make("ALE/" + fname, 
-               render_mode="rgb_array",
-               frameskip = 1, 
-               repeat_action_probability = 0.25)
+'''R-MAX REINFORCEMENT LEARNING'''
 
-# Get maximum (latest) model weights
-file_weights = []
-for file in os.listdir("./output/" + fname + "/"):
-    if file.endswith(".pt") and file.startswith("vqvae"):
-        file_name = file.split(".")[0].split("_")[2]
-        file_weights.append(int(file_name))
-file_weights = np.max(file_weights)
 
-# Import VQ-VAE model
-model_path = f"{path_out}vqvae_weights_{str(file_weights)}.pt"
-try:
-    model = torch.load(model_path, weights_only = False)
-    model.eval()
-    print(f"Loaded model weights from {model_path}")
-except:
-    print(f"Model weights file {model_path} not found.")
-    exit()
 
-# Initialize policy index mapper auxiliary class
-mapper = PolicyIndexMapper()
+# Initialize index mapper auxiliary class
+indexmapper = IndexMapper(fname, state_space_size)
 
-# Initialize necessary arrays
-rewardList      = []
-rewarddeq       = deque([], maxlen = 100)
-avgrewardlist   = []
+# Keep track of reward
+reward_arr = []
 
-# State and action space
+# State space
 nS = state_space_size
+# Action space
 nA = env.action_space.n
 
 # R-max data structures
@@ -167,23 +195,48 @@ n_sa        = np.zeros((nS, nA))
 n_sas       = np.zeros((nS, nA, nS))
 r_sa        = np.zeros((nS, nA))
 
-# State transition matrix
-State_transition = np.zeros((nS, nA, nS))
+# Define state transition matrix
+state_mdp = np.zeros((nS, nA, nS))
+# Set ones in "diagonal"
 for i in range(nS):
-    State_transition[i,:,i] = 1
+    state_mdp[i, :, i] = 1
+# Rewards array ("Rewards")
+reward_mdp  = np.ones((nS, nA)) * R_max
 
-Qs = []
+# Q-values
+Q           = np.zeros((nS, nA))
 
-Q = np.zeros((nS,nA))
+# Previous Q-values
+Q_prev      = np.ones((nS, nA)) * R_max
 
-Q_prev = np.ones((nS,nA)) * R_max
-Reward = np.ones((nS,nA)) * R_max
+# Value function
+V           = np.zeros(state_space_size)
 
-# Initialize value function: all zero
-V = np.zeros(state_space_size)
+# Policy
+policy      = np.zeros(state_space_size, dtype = int)
 
-# Optional: to reconstruct policy later
-policy = np.zeros(state_space_size, dtype=int)
+
+
+'''AUXILIARY FUNCTIONS'''
+
+
+
+# Decide action to take according to policy
+def policy_action(state):
+
+    if np.random.rand() < epsilon:
+        return env.action_space.sample()
+    else:
+        return policy[indexmapper.get_index(state)]
+    
+
+
+def detgetMRP(P_sas, R_sa, pi):
+
+    P_ss = np.squeeze(np.take_along_axis(P_sas, pi.reshape(-1,1,1), axis=1), axis=1)
+    R_s  = np.take_along_axis(R_sa, pi.reshape(-1,1), axis=1)
+
+    return P_ss, R_s
 
 
 
@@ -191,85 +244,118 @@ policy = np.zeros(state_space_size, dtype=int)
 
 
 
+# Clear line
 print()
 
-
+# Episode loop
 for episode in range(epochs):
 
-    # Reset environment and obtain observation
+    # Reset environment
     obs, _ = env.reset()
 
-    # Get frame (no pre-processing)
-    img = Image.fromarray( env.render(), mode = 'RGB' )
+    # Open observed state as image
+    frame = Image.fromarray( env.render(), mode = "RGB" )
 
-    # Convert the frame into code (preprocess, encode and quantize)
-    code = get_index_from_image(img, state_space_size)
+    # Convert frame into code (preprocess, encode, quantize)
+    state_code = indexmapper.get_index_from_image(frame)
 
+    # Get the index in state dictionary corresponding to frame code
+    state = indexmapper.get_index(state_code)
+
+    # Keep track of total reward
+    reward_total = 0
+
+    # Check if done
     done = False
 
-    # Keep track of total reward
-    total_reward = 0
-
+    # Episode loop
     while not done:
 
-        action = policy_action(code) # random policy
+        # Sample action from policy
+        action = policy_action(state_code)
+
+        # Perform step
         obs, reward, terminated, truncated, _ = env.step(action)
+
+        # Check if done
         done = terminated or truncated
-        img_next =  Image.fromarray( env.render(), mode = 'RGB' )
-        next_code = get_index_from_image(img_next, state_space_size)
+
+        # Render and open new frame as image
+        n_frame = Image.fromarray( env.render(), mode = "RGB" )
+
+        # Convert new frame into code
+        n_state_code = indexmapper.get_index_from_image(n_frame)
+
+        # Get the index
+        n_state = indexmapper.get_index(n_state_code)
 
         # Update reward table
-        r_sa[mapper.get_index(code), action] += reward
+        r_sa[state, action] += reward
 
-        n_sa_true[mapper.get_index(code), action] += 1
+        # Update "true" state transition
+        n_sa_true[state, action] += 1
 
-        if (n_sa[mapper.get_index(code),  action] < m):
+        # Check if already visited state-action pair enough times
+        if n_sa[state, action] < m:
 
-            n_sa[mapper.get_index(code),  action] += 1
-            n_sas[mapper.get_index(code), action, mapper.get_index(next_code)] += 1
+            # Update counters
+            n_sa[state, action]             += 1
+            n_sas[state, action, n_state]   += 1
 
         # Update total reward
-        total_reward += reward
+        reward_total += reward
 
-        # Move to next state
-        code = next_code
+        # Update next state
+        state_code = n_state_code
 
     # Construct MDP
 
     epsilon = max(epsilon * epsilon_decay, epsilon_min)
 
-    for state, action in np.ndindex(n_sa.shape):
-        if n_sa[state, action] >= m:
-            for next_state in range(nS):
-                State_transition[state, action, next_state] = n_sas[state, action, next_state] / n_sa[state, action]
-            Reward[state, action] = r_sa[state, action] / n_sa_true[state, action]
+    # Update reward and state transition
+    for s, a, in np.ndindex(n_sa.shape):
+        # If visited enough times
+        if n_sa[s, a] >= m:
+            # For each new possible state
+            for n_s in range(nS):
+                # Update state transitions
+                state_mdp[s, a, n_s] = n_sas[s, a, n_s] / n_sa[s, a]
+            # Update rewards
+            reward_mdp[s, a] = r_sa[s, a] / n_sa_true[s, a]
 
-    rewardList.append(total_reward)
+    # Append total reward to list
+    reward_arr.append(reward_total)
 
-    # Print log to console
-    print(f"Episode {episode}\tTotal reward: {total_reward}")
-    
+    # Print log to console
+    print(f"Episode {episode}\tTotal reward: {reward_total}")
+
     # Solve MDP
 
-    # Keep track of previous policy
+    # Keep track of previous policy
     prev_policy = policy.copy()
 
-    I           = np.eye(State_transition.shape[0])
-    P_ss, R_s   = detgetMRP(State_transition,Reward,policy) # Deterministic 
+    # Identity matrix
+    I           = np.eye(state_mdp.shape[0])
 
-    V       = np.linalg.solve(I - gamma * P_ss, R_s) 
-    Q_prev  = Q.copy()
-    Q       = Reward + gamma * np.squeeze(np.matmul(State_transition, V))
+    P_ss, R_s   = detgetMRP(state_mdp, reward_mdp, policy)
 
-    Qs.append(Q)
+    # Value function
+    V = np.linalg.solve(I - gamma * P_ss, R_s)
 
-    policy = np.argmax(Q, axis=1) # Deterministic
+    # Copy previous Q-value function
+    Q_prev = Q.copy()
+
+    # Compute new Q-value function
+    Q = reward_mdp + gamma * np.squeeze( np.matmul( state_mdp, V ) )
+
+    # Update policy
+    policy = np.argmax(Q, axis = 1)
 
     # Plot reward over time
     if episode % 5 == 0:
         
         plt.figure(figsize=(10, 5))
-        plt.plot(rewardList, label='Reward per episode')
+        plt.plot(reward_arr, label='Reward per episode')
         plt.xlabel('Episode')
         plt.ylabel('Total Reward')
         plt.title('Total Reward per Episode')
@@ -277,4 +363,4 @@ for episode in range(epochs):
         plt.savefig(f'{path_out}rmax_reward.pdf')
 
     # Save output to file after each epoch
-    np.save(file = path_out + "rmax_log", arr = rewardList)
+    np.save(file = path_out + "rmax_log", arr = reward_arr)
